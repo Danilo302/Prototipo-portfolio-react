9fa15485dc48db5f09cce9fee4318bdd6d6284e8
Danilo <danilofelix827@gmail.com>
2025-05-21 23:32:50 -0300

    estilizando sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/Danilo302.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragraph from '../../components/Paragraph'
 import Title from '../../components/Title'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Danilo302.png" />
-    <Title fontSize={20}>Danilo Felix</Title>
+    <SidebarContainer>
+      <Avatar></Avatar>
+      <Title fontSize={20}>Danilo Felix</Title>
+      <Paragraph tipo="secundario" fontSize={16}>
+        Danilo302
+      </Paragraph>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor FullStack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
